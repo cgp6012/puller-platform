3ec1f6ce04012c7049e82b3b5cfc8d1a9270b2d5
cgp6012 <cgp6012@gmail.com>
2020-03-31 15:46:57 -0400

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://carson:Goheels#22@puller-shard-00-00-qjog7.mongodb.net:27017,puller-shard-00-01-qjog7.mongodb.net:27017,puller-shard-00-02-qjog7.mongodb.net:27017/test?ssl=true&replicaSet=Puller-shard-0&authSource=admin&retryWrites=true&w=majority" 
+  };
