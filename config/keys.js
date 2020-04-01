345bab5e6144d0cf9a339d52bf4b27fc5fb738f7
cgp6012 <cgp6012@gmail.com>
2020-04-01 09:50:05 -0400

    backend setup

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://carson:Goheels#22@puller-shard-00-00-qjog7.mongodb.net:27017,puller-shard-00-01-qjog7.mongodb.net:27017,puller-shard-00-02-qjog7.mongodb.net:27017/test?ssl=true&replicaSet=Puller-shard-0&authSource=admin&retryWrites=true&w=majority" 
+    mongoURI: "mongodb://carson:Goheels#22@puller-shard-00-00-qjog7.mongodb.net:27017,puller-shard-00-01-qjog7.mongodb.net:27017,puller-shard-00-02-qjog7.mongodb.net:27017/test?ssl=true&replicaSet=Puller-shard-0&authSource=admin&retryWrites=true&w=majority",
+    secretOrKey: "secret" 
   };
