4f4a534b44a51ea279a9148f8f9755a40f241451
cgp6012 <cgp6012@gmail.com>
2020-04-03 09:50:25 -0400

    heroku attempt 2

--- server.js
+++ server.js
@@ -30,6 +30,9 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
 //serve static assets if in production
 if(process.env.NODE_ENV === 'production'){
   //set static folder
@@ -39,6 +42,3 @@ if(process.env.NODE_ENV === 'production'){
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
